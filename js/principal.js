9d8c56b58276d6b5bfcb24665f067b8fd3dfe7f3
Melissa Junqueira <meljblins@gmail.com>
2021-05-18 10:27:10 -0300

    Adicionando estilo à página

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nutricionista Aparecida";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var linha = 0; linha < pacientes.length; linha++){
+    
+    var paciente = pacientes[linha];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida){
+        var imc = (peso)/(altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
