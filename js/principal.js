2aea069af89926ce9c5aa3d4cb5528b66e390562
Melissa Junqueira <meljblins@gmail.com>
2021-05-19 09:17:46 -0300

    Adicionando pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,10 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nutricionista Aparecida";
 
+titulo.addEventListener("click", function(){
+    console.log("Olá, meu nome é Aparecida e esta é minha página!");
+});
+
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var linha = 0; linha < pacientes.length; linha++){
@@ -37,3 +41,37 @@ for (var linha = 0; linha < pacientes.length; linha++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var formulario = document.querySelector("#adicionar-formulario");
+
+    var nome = formulario.nome.value;
+    var peso = formulario.peso.value;
+    var altura = formulario.altura.value;
+    var gordura = formulario.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
