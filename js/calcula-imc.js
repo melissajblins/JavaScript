1055291b987df2af2f72e46b702cd4d0036e1472
Melissa Junqueira <meljblins@gmail.com>
2021-05-20 10:06:01 -0300

    Validando o formulário

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -19,17 +19,17 @@ for (var linha = 0; linha < pacientes.length; linha++){
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000){
+    if (!pesoEhValido){
         console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.00){
+    if (!alturaEhValida){
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida!";
@@ -41,6 +41,23 @@ for (var linha = 0; linha < pacientes.length; linha++){
         tdImc.textContent = imc;
     }
 }
+
+function validaPeso(peso){
+    if (peso >= 0 && peso < 1000){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if (altura>= 0 && altura < 3.0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 function calculaImc(peso, altura){
     var imc = 0;
--- js/form.js
+++ js/form.js
@@ -6,13 +6,25 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var paciente = obtemPacienteDoFormulario(formulario);
 
-    var pacienteTr = montaTr(paciente)
+    var pacienteTr = montaTr(paciente);
+
+    erros = validaPaciente(paciente);
+
+    console.log(erros);
+
+    if (erros.length > 0){
+        exibeMensagensDeErro(erros);
+        return;
+    }
 
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     formulario.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErros.innerHTML = "";
 });
 
 function obtemPacienteDoFormulario(formulario){
@@ -47,4 +59,47 @@ function montaTd(dado, classe){
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente){
+    
+    var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("Preencha o nome do paciente!");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("Preencha o peso do paciente!");
+    }
+
+    if(!validaPeso(paciente.peso)) {
+        erros.push("Peso inválido!");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("Preencha a altura do paciente!");
+    }
+
+    if(!validaAltura(paciente.altura)) {
+        erros.push("Altura inválida!");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("Preencha a gordura do paciente!");
+    }
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
