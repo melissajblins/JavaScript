079a63ef0b56c1b0f30410245d0fa5b0b020e0db
Melissa Junqueira <meljblins@gmail.com>
2021-05-21 09:15:09 -0300

    Removendo pacientes

---
+++ js/remover-paciente.js
@@ -0,0 +1,9 @@
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fade-out");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+});
